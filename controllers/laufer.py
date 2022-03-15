c13220418945c64555cd0c5a4039810ce28bc140
Benjamin Balcinovic <benjamin.balcinovic@bulme.at>
2022-03-15 10:28:31 +0100

    laufer.py erstellt und bearbeitet

---
+++ controllers/laufer.py
@@ -0,0 +1,13 @@
+from flask import Flask,request
+from flask.templating import render_template
+from flask import Blueprint
+import sqlalchemy
+from models import db, Marathon
+
+laufer_blueprint = Blueprint('laufer_blueprint', __name__)
+
+@laufer_blueprint.route("/laufer")
+def index():
+    session : sqlalchemy.orm.scoping.scoped_session = db.session
+    laufer = session.query(Marathon).all()
+    return render_template("laufer.html", items = laufer)
