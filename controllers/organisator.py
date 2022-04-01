4f04ecaa3987261e01538383b1231458aa7c755b
Benjamin Balcinovic <benjamin.balcinovic@bulme.at>
2022-04-01 10:38:36 +0200

    delete button bearbeitet

--- controllers/organisator.py
+++ controllers/organisator.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, flash
 from flask.templating import render_template
 from flask import Blueprint
 import sqlalchemy
 from models import db, Organisator
 from Forms.addOrganisator import AddOrganisatorForm
+from Forms.deleteOrganisatorForm import DeleteOrganisatorForm
 
 organisator_blueprint = Blueprint('organisator_blueprint', __name__)
 
@@ -33,3 +34,21 @@ def index():
 
     marathonlauf = db.session.query(Organisator).all()
     return render_template("organisator.html", form = addOrganisatorFormObject, items = marathonlauf)
+
+organisator_blueprint.route("/organisator/delete", methods=["post"])
+def deleteOrganisator():
+    deleteOrganisatorFormObj = DeleteOrganisatorForm()
+    if deleteOrganisatorFormObj.validate_on_submit():
+        print("gültig")
+
+        OrganisatorIdToDelete = deleteOrganisatorFormObj.itemId.data
+        OrganisatorToDelete = db.session.query(Organisator).filter(Organisator.MarathonID == OrganisatorIdToDelete)
+        OrganisatorToDelete.delete()
+        
+        db.session.commit()
+    else:
+        print("Fatal Error")
+    
+    flash(f"Organisator with id {OrganisatorIdToDelete} has been deleted")    
+
+    return redirect("/organisator")
